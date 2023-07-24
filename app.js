92f14773555caf44b66cb6ca8d2553bfa520f671
Dina <dina.yamanid@gmail.com>
2023-07-24 13:22:40 +0200

    first commit -m

---
+++ app.js
@@ -0,0 +1,18 @@
+//Background
+VANTA.FOG({
+    el: "body",
+    mouseControls: true,
+    touchControls: true,
+    gyroControls: false,
+    minHeight: 1000,
+    minWidth: 200.00,
+    highlightColor: 0x118658,
+    midtoneColor: 0xe5e378,
+    lowlightColor: 0xc5156d,
+    zoom: 3.00
+  })
+
+ 
+  //typed.js
+
+
